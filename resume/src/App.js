b4586c41905e0ed780acf5607d9a2f9a72a58ba8
yasersyed <sysyed@syr.edu>
2021-04-24 23:12:48 -0500

    header sample

--- resume/src/App.js
+++ resume/src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <H1> Yaser Resume Project</H1>
       </header>
     </div>
   );
